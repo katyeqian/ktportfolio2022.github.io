b362c2866b2838f3cf7b3f0be59ae5f73206fd8f
katie chen <katie.q.chen@gmail.com>
2025-02-27 23:05:27 -0500

    update 2025
    - update nav on all projects
    - add datadog
    - change landing page bio
    - change about page

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
         $('.hideme').each( function(i){
             
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_object = $(this).position().top + ($(this).outerHeight())/3;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
